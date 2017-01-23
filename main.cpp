8d455a34ead0b49d0dee34da7fa3cce847e4eb11
medeaeva <ferencz.eva.medea@gmail.com>
2017-01-23 13:12:47 +0100

    final push

--- main.cpp
+++ main.cpp
@@ -1,29 +1,9 @@
-#include <iostream>
-#include <string>
-#include <fstream>
+#include "Arguments.h"
 
-using namespace std;
+int main(int argc, char** argv) {
 
-int main(int argc, char* argv[]) {
-  if (argc != 2) {
-    cout << "Input should be of the form 'count filename.txt'";
-    return 1;
-  }
-  else {
-    ifstream input_file(argv[1]);
-    if (!input_file) {
-      cerr << "No file provided.";
-      return 0;
-    }
-    char c;
-    int count = 0;
-    while (input_file.get(c)) {
-      if (c == '\n') {
-        count++;
-      }
-    }
-    cout << "Total lines in file: " << count;
-  }
+  Arguments arguments(argc, argv);
+  arguments.run();
   return 0;
 }
 
